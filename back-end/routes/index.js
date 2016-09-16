aa6b22a484662301343c80b33a76f6d7119804a8
PirieD704 <dp75975@gmail.com>
2016-09-16 13:27:54 -0400

    ecommerce app of shoe store

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,126 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var mongoUrl = "mongodb://localhost:27017/ecommerce";
+var User = require('../models/account')
+mongoose.connect(mongoUrl);
+// Include bcrypt to store hashed pass
+var bcrypt =  require('bcrypt-nodejs')
+var randToken = require('rand-token').uid;
+//This is our config mudule. We have access to
+//config.secretTestKey
+var configVars = require('../config'); 
+var stripe = require("stripe")("configVars.secretTestKey");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+	User.find({}, (error,documents) =>{
+		if(error){
+			res.json(error);
+		}else{
+			res.json(documents);
+		}
+	})
+	res.render('index', { title: 'Express' });
 });
+
+router.post('/addAccount', (req, res, next) => {
+	
+	var token = randToken(32);
+	console.log(token);
+    var newUser = new User({
+        username: req.body.username,
+        password: bcrypt.hashSync(req.body.password),
+        email: req.body.email,
+        token: token,
+		// Add tokenExpDate
+    });
+
+
+    newUser.save((error, documentAdded) => {
+    	if(error){
+    		res.json({
+    			message: error    		
+    		})
+    	}else{
+    		res.json({
+        		message: "added",
+        		token: token
+    		});
+    	}
+    });
+
+});
+
+router.post('/stripe', (req, res, next) => {
+
+	stripe.charges.create({
+		amount: req.body.amount,
+		currency: "usd",
+		source: req.body.stripeToken,
+		description: "Charge for " + req.body.email 
+	}).then((charge) => {
+		res.json({
+			message: "paid"
+		})
+	}, (err) => {
+		res.json({
+			message: "failedPayment"
+		})
+	});
+})
+
+router.post('/login', (req, res, next) => {
+	User.findOne(
+		{username: req.body.username},  // This is the droid we're looking for
+		function(error, document){
+			//document is the document returned from our Mongo query.. ie, the droid.
+			//The document will have a property for each field. WE need to check the password field
+			//in the DB against the hashed bcrypt version
+			if(document == null){
+				//No match
+				res.json({failure: "noUser"});
+			}else{
+				var loginResult = bcrypt.compareSync(req.body.password, document.password);
+				if(loginResult){
+					//The password is correct. Log them in.
+					res.json({
+						success: "userFound"
+					});
+				}else{
+					//hashes did not match or the doc wasn't found. Goodbye
+					res.json({
+						failure: "badPass"
+					})
+				}
+			}
+		}
+	)
+})
+
+router.get('getUserData', (req, res, next) => {
+	var userToken = req.query.token; // the XXX in ?token=XXXXX
+	if(userToken == undefined){
+		//No token was supplied
+		res.json({failure: "noToken"})
+	}else{
+		User.findOne(
+			{token: userToken}, // This is the droid we're looking for
+			(error, document) => {
+				if(document == null){
+					//this token is not in the system
+					res.json({failure: 'badToken'}); //Angular will need to act on this information
+				}else{
+					res.json({
+						username: document.username,
+						grind: document.grind,
+						frequency: document.frequency,
+						token: document.token
+					});
+				}
+			}
+		)
+	}
+})
+
 
 module.exports = router;
---
+++ front-end/js/mainController.js
@@ -0,0 +1,121 @@
+var ecommerceApp = angular.module("ecommerceApp", ['ngRoute', 'ngCookies']);
+ecommerceApp.controller('ecommerceController', function($scope, $rootScope, $http, $timeout, $location, $cookies) {
+
+	var apiPath = "http://localhost:3000";
+
+	$scope.register = () => {
+		// if($scope.password != $scope.password2){
+		// 	console.log()
+		// }
+		$http.post(apiPath + '/addAccount', {
+			username: $scope.username,
+			password: $scope.password,
+			password2: $scope.password2,
+			email: $scope.email
+		}).then(function successCallback(response){
+			// console.log(response)
+			if(response.data.message == "added"){
+				$cookies.put('token', response.data.token) //Will be used for validation
+				$cookies.put('username', $scope.username) //Will be used strictly for info purposes
+				$location.path('/options');
+			}else{
+				console.log(response.data.message)
+			}
+		}, function errorCallback(response){
+			console.log('fail');
+			console.log(response);
+		});
+
+	};
+
+	// $http.get(apiPath + '/getUserData?token=' + $cookies.get('token'))
+	// .then(function successCallback(response){
+	// 	response.data.xxxxx = whatever res.json was in express.
+	// 	if(response.data.failure == 'badToken'){
+	// 		$location.path = '/login' //Goodbye
+	// 	}else if(response.data.failure == 'noToken'){
+	// 		$location.path = '/login' //No token. Goodbye
+	// 	}else{
+	// 		//the token is good. Response.data will have their stuff in it.
+	// 		$scope.username = response.data.username;
+	// 		$scope.grindType = response.data.grindType;
+	// 	}
+	// }, function errorCallback(response){
+	// 	// console.log(response)
+	// });
+
+	$scope.login = () => {
+		$http.post(apiPath + '/login', {
+			username: $scope.username,
+			password: $scope.password
+		}).then(function successCallback(response){
+			console.log(response.data);
+			if(response.data.success == "userFound"){
+				$cookies.put('token', response.data.token)
+				$cookies.put('username', $scope.username)
+				$location.path('/options');
+			}
+		}, function errorCallback(response){
+			console.log(response.data);
+		})
+	}
+
+	$scope.payOrder = function(userOptions) {
+	    $scope.errorMessage = "";
+	    var handler = StripeCheckout.configure({
+	        key: publicTestKey,
+	        image: 'assets/img/dc_roasters_200x124_lt.png',
+	        locale: 'auto',
+	        token: function(token) {
+	            console.log("The token Id is: ");
+	            console.log(token.id);
+
+	            $http.post(apiUrl + '/stripe', {
+	                amount: $scope.total * 100,
+	                stripeToken: token.id,
+	                token: $cookies.get('token')
+	                    //This will pass amount, stripeToken, and token to /payment
+	            }).then(function successCallback(response) {
+	                console.log(response.data);
+	                if (response.data.success) {
+	                    //Say thank you
+	                    $location.path('/receipt');
+	                } else {
+	                    $scope.errorMessage = response.data.message;
+	                    //same on the checkout page
+	                }
+	            }, function errorCallback(response) {});
+	        }
+	    });
+	    handler.open({
+	        name: 'DC Roasters',
+	        description: 'A Better Way To Grind',
+	        amount: $scope.total * 100
+	    });
+	};
+})
+
+// Set up routes using the routes module
+ecommerceApp.config(($routeProvider) => {
+	$routeProvider.when('/',{
+		templateUrl: 'views/main.html',
+		controller: 'ecommerceController'
+	}).when('/login',{
+		templateUrl: 'views/login.html',
+		controller: 'ecommerceController'
+	}).when('/register',{
+		templateUrl: 'views/register.html',
+		controller: 'ecommerceController'
+	}).when('/options',{
+		templateUrl: 'views/options.html',
+		controller: 'ecommerceController'
+	}).when('/delivery',{
+		templateUrl: 'views/delivery.html',
+		controller: 'ecommerceController'
+	}).when('/payment',{
+		templateUrl: 'views/payment.html',
+		controller: 'ecommerceController'
+	}).otherwise({
+		redirectTo: '/'
+	})
+})
